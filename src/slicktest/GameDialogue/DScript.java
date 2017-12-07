0e147e96c0d958aa21d010775c5b5f6e10dba07e
eProw <jasansu@gmail.com>
2017-12-08 00:12:21 +0100

    Terminada primera version de sistema de dialogo: -Añadidos sonidos -Algunos bugs eliminados

--- src/slicktest/GameDialogue/DScript.java
+++ src/slicktest/GameDialogue/DScript.java
@@ -3,8 +3,8 @@ package slicktest.GameDialogue;
 public abstract class DScript {
     public static String[] string ={
         "HOLA MUNDO!\n"+
-            "Esto es una prueba visual para comprobar que el  maravilloso sistema de dialogo funciona tal y como se esperaba. Como puedes "+
-            "ver, es bastante parecido a como se ve en las consolas portatiles de doble pantalla tactil...",
+            "Esto es una prueba visual creada por el parguela de jose, para comprobar que el maravilloso sistemade dialogo funciona tal y como se esperaba. Como  puedes "+
+            "ver, es bastante parecido a como se ve en  las consolas portatiles de doble pantalla...\nPINOCHO JAJA EQUISDE....",
         "/switch/"
     };
 }
